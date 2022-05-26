5f607549ef48b887cdb602e2de17717610856815
unknown <nathaniel2048@gmail.com>
2022-05-26 22:02:13 +0800

    First commit of Helping Hand

---
+++ src/features/intangibleRequest/intangibleRequestSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const intangibleRequestSlice = createSlice({
+  name: "intangibleRequest",
+  initialState,
+  reducers: {},
+});
+
+export default intangibleRequestSlice.reducer;
---
+++ src/features/tangibleRequest/tangibleRequestSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const tangibleRequestSlice = createSlice({
+  name: "tangibleRequest",
+  initialState,
+  reducers: {},
+});
+
+export default tangibleRequestSlice.reducer;
